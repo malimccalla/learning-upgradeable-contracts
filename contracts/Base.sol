ca7c10265a7d2f620845d7e2e8c7da4c5f451c99
Mali McCalla <malimccalla@gmail.com>
2022-03-26 14:25:54 +0000

    add v3

--- contracts/Base.sol
+++ contracts/Base.sol
@@ -13,6 +13,12 @@ contract Base is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgrad
         // As this is not a standard contrctor function we can use this funny syntax
         // to pass the arguments to what would have been our ERC20("Base", "BASE") constructor syntax
         __ERC20_init("Base", "BASE");
+
+        // We also need to manually inialize the ownable contract we inherit from
+        // Without this the owner variable would not be initalized and the upgrade function
+        // Would not be able to run as _authorizeUpgrade requires onlyOwner
+        __Ownable_init();
+
         _mint(msg.sender, 10000000 * 10 ** decimals());
     } 
 
@@ -24,7 +30,21 @@ contract Base is Initializable, ERC20Upgradeable, UUPSUpgradeable, OwnableUpgrad
 // To make a v2 of our contract we extend v2. This gives us all the functionailty of v1
 // but with the ability to add new functions
 contract BaseV2 is Base {
-    function version() public pure returns (string memory) {
+    uint public fee;
+
+    // We know that we will want to change this function in the future so we mark
+    // it as 'virtual'. This allows us to override it with the 'override' keyword
+    function version() public pure virtual returns (string memory) {
         return "v2";
+    }
+}
+
+// In version 3 we should extend v2. 
+contract BaseV3 is BaseV2 {
+    uint public tax;
+
+    // At this point the function should be both virtual and override
+    function version() public pure override virtual returns (string memory) {
+        return "v3";
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -5,12 +5,24 @@ describe("Base", function () {
   it("deploys", async function () {
     const Base = await hre.ethers.getContractFactory("Base");
     const BaseV2 = await hre.ethers.getContractFactory("BaseV2");
+    const BaseV3 = await hre.ethers.getContractFactory("BaseV3");
 
     // Without the { kind: "uups" } we would default to using the transparent proxy pattern
     const base = await hre.upgrades.deployProxy(Base, { kind: "uups" });
 
     expect(await base.name()).to.equal("Base");
 
-    await hre.upgrades.upgradeProxy(base, BaseV2);
+    // --- time passes, build out features, get funding...
+    // Oh we need to write more features in our smart contract
+
+    // So we call upgradeProxy
+    const baseV2 = await hre.upgrades.upgradeProxy(base, BaseV2);
+
+    // -- time passes again
+    expect(await baseV2.version()).to.equal("v2");
+
+    const baseV3 = await hre.upgrades.upgradeProxy(base, BaseV3);
+
+    expect(await baseV3.version()).to.equal("v3");
   });
 });
